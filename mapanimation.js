e89ddda1b80876ba9b9e6e1cbff81790bd55442e
Crescens <2e3crescens@gmail.com>
2023-11-11 17:40:12 +0800

    added marker

--- mapanimation.js
+++ mapanimation.js
@@ -7,32 +7,9 @@ function initMap () {
     
     // Creation of Map object
     map = new google.maps.Map(document.getElementById("map"), options)
+
+    // Creation of Marker object
+    let singapore = {lat: 1.3853796796577809, lng: 103.74487128465967};
+    marker = new google.maps.Marker({position: singapore, map: map});
 }
 
-// console.log('connected');
-// // Initialize and add the map
-// let map;
-
-// async function initMap() {
-//   // The location of Singapore
-//   const position = { lat: -1.3825533620627133, lng: 103.7430026798573 };
-//   // Request needed libraries.
-//   //@ts-ignore
-//   const { Map } = await google.maps.importLibrary("maps");
-//   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-//   // The map, centered at Singapore
-//   map = new Map(document.getElementById("map"), {
-//     zoom: 4,//     center: position,
-//     mapId: "DEMO_MAP_ID",
-//   });
-
-//   // The marker, positioned at Uluru
-//   const marker = new AdvancedMarkerElement({
-//     map: map,
-//     position: position,
-//     title: "Singapore",
-//   });
-// }
-
-// initMap();
