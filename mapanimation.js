34e8204fe3533985bbf7e6412035be6bc915b879
Crescens <2e3crescens@gmail.com>
2024-01-05 15:07:13 +0800

    idk what this is

--- mapanimation.js
+++ mapanimation.js
@@ -13,3 +13,41 @@ function initMap () {
     marker = new google.maps.Marker({position: singapore, map: map});
 }
 
+
+// run() and getBusLocations() calls api and returns quried data in console respectively
+async function run(){
+    // get bus data    
+	const locations = await getBusLocations();
+	console.log(new Date());
+	console.log(locations);
+
+	// timer
+	setTimeout(run, 15000);
+}
+
+// Request bus data from datamall
+async function getBusLocations(){
+    var myHeaders = new Headers();
+    myHeaders.append("AccountKey", "jjGDI3/TTG6wPZBatS96ww==");
+    // myHeaders.append("Host", "http://127.0.0.1:5500");
+    // myHeaders.append("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+
+    var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+    };
+
+    // fetch("http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=44461&ServiceNo=188", requestOptions)
+ fetch("https://reqres.in/api/users", requestOptions)
+    .then(result => result.json())
+    .then(data => console.log(JSON.parse(data)));
+    // .catch(error => console.log('error', error));
+}
+getBusLocations();
+
+// run();
+
+//Goal: (DISPLAY markers) of (BUS 188) of (user selected timing)
+// If (tracking bus: refresh run every 5s)
+// If
